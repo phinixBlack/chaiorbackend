32adc91fdac37792794f829741594dbbf907bc3a
phenixDigibeee <139323533+phenixDigibeee@users.noreply.github.com>
2023-11-29 10:54:06 +0530

    multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,7 @@
+import {v2 as cloudinary} from 'cloudinary';
+import {fs} from 'fs'
+cloudinary.config({ 
+  cloud_name: 'dpgrfr2mo', 
+  api_key: '179267632715674', 
+  api_secret: '***************************' 
+});
